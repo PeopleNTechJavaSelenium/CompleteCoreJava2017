f624a31c3fdd8ea517ad5420a765eb2bf98d4e20
mrahman <rahmanww@gmail.com>
2017-04-09 15:16:11 -0400

    array,enum,loops,api

---
+++ src/loops/UseWhileLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class UseWhileLoop {
+
+	public static void main(String[] args) {
+
+		int num=9;
+		while(num<9){
+			System.out.println("Hello World");
+			num++;
+		}
+	}
+}
---
+++ src/switchstatement/ExecuteDay.java
@@ -0,0 +1,10 @@
+package switchstatement;
+
+public class ExecuteDay {
+
+	public static void main(String[] args) {
+		Week week = new Week();
+        week.taskToDo(Day.FRIDAY);
+	}
+
+}
